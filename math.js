f727a3405c32eb400bcb38a4921d4a4c0fb16a43
ys558 <yuyi.gz@163.com>
2020-07-14 11:19:38 +0800

    jest.config.js的生成，es语法的改写配置.babelrc文件

--- math.js
+++ math.js
@@ -1,9 +1,10 @@
-const add = (a, b) => a+b
-const minus = (a, b) => a-b
+// 2. 将mommonjs的导出方式改为es的导出方式：
+export const add = (a, b) => a+b
+export const minus = (a, b) => a-b
 
-// 这里套上try/catch为了在浏览器运行，实际上项目中, 多用前端框架，如react，vue， 本身即模块化开心，不需要写try/catch：
-try{
-  module.exports = {
-    add, minus
-  }
-}catch (e){}
+// // 1. 这里套上try/catch为了在浏览器运行，实际上项目中, 多用前端框架，如react，vue， 本身即模块化开心，不需要写try/catch：
+// try{
+//   module.exports = {
+//     add, minus
+//   }
+// }catch (e){}
--- math.test.js
+++ math.test.js
@@ -1,5 +1,11 @@
-const math = require('./math')
-const {add, minus} = math
+// const math = require('./math')
+// const {add, minus} = math
+
+// 2. 将mommonjs的导出方式改为es的导出方式：
+// 此时需安装：nyarn add @babel/core@7.4.5 @babel/preset-env@7.4.5 -D
+// 再创建.bablerc
+import math from './math'
+import {add, minus} from './math'
 
 test('测试加法3+7', ()=> expect(add(3,7)).toBe(10))
 test('测试加法3-3', ()=> expect(minus(3,3)).toBe(0))
