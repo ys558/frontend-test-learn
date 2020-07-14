a5dcd522f6d76171e51f047d3b90a2189ad31bf5
ys558 <yuyi.gz@163.com>
2020-07-14 10:33:59 +0800

    为了让浏览器运行，在模块化的math.js代码上套上try/catch函数，以免报错

--- math.js
+++ math.js
@@ -1,6 +1,9 @@
 const add = (a, b) => a+b
 const minus = (a, b) => a-b
 
-module.exports = {
-  add, minus
-}
+// 这里套上try/catch为了在浏览器运行，实际上项目中, 多用前端框架，如react，vue， 本身即模块化开心，不需要写try/catch：
+try{
+  module.exports = {
+    add, minus
+  }
+}catch (e){}
