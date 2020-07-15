60e239fb21ddeb738942abe4de7d24a97fbb32ae
ys558 <yuyi.gz@163.com>
2020-07-15 10:36:39 +0800

    生命周期钩子函数

---
+++ l7_lifecycle_hook_func/Counter.js
@@ -0,0 +1,10 @@
+export default class Counter {
+  constructor() {
+    this.number = 0
+  }
+  add1(){ this.number += 1 }
+  minus1(){ this.number -= 1 }
+  
+  add2(){ this.number += 2 }
+  minus2(){ this.number -= 2 }
+}
